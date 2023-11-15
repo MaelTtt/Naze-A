0ab0dfeb4f8b47d987ed1c97adb69e784cd21177
maelttt <mael.turchetto@orange.fr>
2023-11-15 08:15:47 +0100

    Initial Commit

---
+++ Naze A/Naze_AApp.swift
@@ -0,0 +1,17 @@
+//
+//  Naze_AApp.swift
+//  Naze A
+//
+//  Created by turchetto mael on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Naze_AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
